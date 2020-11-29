931d1408a68ac7f755acac40da8924484029c87c
alone-wolf <1031629044@qq.com>
2020-11-30 00:24:36 +0800

    重命名文件

--- app/src/main/java/com/wh/mymusic/activity/MainActivity.java
+++ app/src/main/java/com/wh/mymusic/activity/MainActivity.java
@@ -1,17 +1,14 @@
 package com.wh.mymusic.activity;
 
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.permissionx.guolindev.PermissionX;
 import com.wh.mymusic.PlayService;
@@ -22,14 +19,12 @@ import com.wh.mymusic.util.Util;
 import com.wh.mymusic.viewmodel.MediaListViewModel;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends BaseMainActivity {
@@ -127,8 +122,7 @@ public class MainActivity extends BaseMainActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.side_drawer_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
